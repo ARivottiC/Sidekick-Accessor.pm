0f81e753f726bede7b2b3e8dde2f496733c1310a
André Rivotti Casimiro <andre.r.casimiro@gmail.com>
2014-08-14 15:51:01 +0100

    startup

---
+++ README.md
@@ -0,0 +1,19 @@
+# NAME
+
+Sidekick::DataType - we'll get there
+
+# VERSION
+
+version 0.0.1
+
+# AUTHOR
+
+André Rivotti Casimiro <rivotti@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by André Rivotti Casimiro.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
---
+++ lib/Sidekick/DataType.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+package Sidekick::DataType;
+
+use strict;
+use warnings;
+
+use Hash::Util::FieldHash ();
+
+Hash::Util::FieldHash::fieldhash my %Hash;
+
+use overload
+    '%{}'      => sub { return { %{ $Hash{ shift() } } } },
+    'fallback' => 1;
+
+sub new {
+    my $class = shift;
+    my %arg   = @_;
+
+    my $self  = bless \( my $o ), ref $class || $class;
+
+    $Hash{ $self } = { %arg };
+
+    return $self;
+}
+
+1;
+# ABSTRACT: we'll get there
+# vim:ts=4:sw=4:syn=perl
